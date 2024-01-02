d1e8e386e7a9be13c66abcabc86b6f38067b23ac
jynxxxxx <137763251+jynxxxxx@users.noreply.github.com>
2024-01-02 17:50:28 +0900

    change layout of site

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,13 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
-import MainLayout from './components/MainLayout';
-import Home from './pages/Home';
-import Projects from './pages/Projects'
-import AboutMe from './pages/AboutMe';
-import { Contact } from './pages/Contact';
+import MainLayout from './pages/MainLayout';
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path='projects' element={<Projects />} />
-          <Route path='aboutme' element={<AboutMe />} />
-          <Route path='contact' element={<Contact />} />
-        </Route>
+        <Route path="/" element={<MainLayout />}></Route>
       </Routes>
     </>
   );
 }
-
-
-{/* <CartContextProvider>
-        <Routes>
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={<Home />} />
-            <Route path="products">
-              <Route index element={<AllProducts />} />
-              <Route path=":title" element={<ProductDetails />} />
-            </Route>
-            <Route path=":category" >
-              <Route index element={<Categories />} />
-              <Route path=":title" element={<ProductDetails />} />
-            </Route>
-            <Route path="checkout" element={<Checkout />} />
-            <Route path="search" element={<Search />} />
-          </Route>
-        </Routes>
-      </CartContextProvider> */}
---
+++ src/pages/MainLayout.tsx
@@ -0,0 +1,55 @@
+import Home from "../sections/Home";
+import AboutMe from "../sections/AboutMe";
+import Projects from "../sections/Projects";
+import { Contact } from "../sections/Contact";
+import '../App.css'
+import NavBar from "../components/NavBar";
+import { useEffect, useState } from "react";
+
+export default function MainLayout() {
+  const [activeLink, setActiveLink] = useState('home');
+  const [headerStyle, setHeaderStyle] = useState({
+    backgroundImage: 'linear-gradient(to left, rgb(241, 63, 107), rgb(247, 132, 78))'
+  });
+
+  const handleNavLinkClick = (link: string) => {
+    setActiveLink(link);
+  };
+
+  useEffect(() => {
+    setHeaderStyle({
+      backgroundImage: getBackgroundColor(activeLink),
+    });
+
+    console.log(headerStyle)
+  }, [activeLink]);
+
+  const getBackgroundColor = (link: string) => {
+    switch (link) {
+      case 'home':
+        return 'linear-gradient(to right, #ED1E79, #662D8C)';
+      case 'about':
+        return 'linear-gradient(to right, #662D8C, #4e94ca)';
+      case 'projects':
+        return 'linear-gradient(to right, #4e94ca, #7fd8e7)';
+      case 'contact':
+        return 'linear-gradient(to right, #7fd8e7, #0C03BA)';
+      default:
+        return 'default color';
+    }
+  };
+  return (
+    <div className="masterctn">
+      <div className="header" style={headerStyle}>
+        <NavBar activeLink={activeLink} handleNavLinkClick={handleNavLinkClick} />
+      </div>
+
+      <div className="content" style={{ minHeight: '60vh' }}>
+        <Home id="home" />
+        <AboutMe id="about" />
+        <Projects id="projects" />
+        <Contact id="contact" />
+      </div>
+    </div>
+  );
+}
