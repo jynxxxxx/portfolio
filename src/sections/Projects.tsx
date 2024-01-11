e1d58b9d1c38d364a2ce58f65efb93b8f90a024e
jynxxxxx <137763251+jynxxxxx@users.noreply.github.com>
2024-01-11 18:15:35 +0900

    add project slide animation

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -2,6 +2,20 @@ import { ProjectDetailsList } from "../components/Projects";
 import '../style/projects.css'
 
 export default function Projects({ id }: { id: string }) {
+  const rows = document.querySelectorAll('.projectrow');
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      entry.target.classList.toggle('row-animation', entry.isIntersecting)
+    });
+  },
+    {
+      rootMargin: '10% 0%'
+    })
+
+  rows.forEach(row => {
+    observer.observe(row)
+  });
 
   return (
     <section id={id} className='page'>
