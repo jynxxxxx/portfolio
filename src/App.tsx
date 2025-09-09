989d33a5c4d2e95e9ce0871f663f88d75f1b5e71
jynxxxxx <137763251+jynxxxxx@users.noreply.github.com>
2025-09-09 18:25:08 +0900

    add toast sonner

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home';
+import { Toaster } from 'sonner';
 
 export default function App() {
   return (
     <>
+      <Toaster position="top-center" />
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
